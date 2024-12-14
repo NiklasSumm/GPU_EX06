1102a5b198bfb5ff37a876d13a7c755978e20537
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-12-14 19:19:36 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -265,7 +265,7 @@ main(int argc, char * argv[])
 			  << "***    Time for Reduction: " << 1e3 * kernelTimer.getTime()
 				  << " ms" << std::endl
 			  << "***    Sustained bandwidth: " << 1e-9 * kernelTimer.getBandwidth(numElements * sizeof(*d_dataIn))
-				  << " ms" << std::endl
+				  << " GB/s" << std::endl
 			  << "***" << std::endl;
 
 	// Free Memory
