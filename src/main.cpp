03a434366f4d8e7159e656ec697218ad124e1505
NiklasSumm <42118513+NiklasSumm@users.noreply.github.com>
2024-12-14 17:34:50 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -180,9 +180,9 @@ main(int argc, char * argv[])
 		gridSize = ceil(static_cast<float>(numElements) / static_cast<float>(blockSize));
 	}
 
-	//if (gridSize > 8192){
-	//	gridSize = 8192;
-	//}
+	if (gridSize > 1024){
+		gridSize = 1024;
+	}
 
 	cudaMalloc(&d_intermediateSums, 
 		static_cast<size_t>(gridSize * sizeof(*d_intermediateSums)));
